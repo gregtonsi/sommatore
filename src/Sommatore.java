652fee27aacd29d952b486143c71973334568c0b
Tonsi davide <tonsidavide@gmail.com>
2018-02-19 09:29:03 +0100

    aggiunta somma20 e somma100

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -6,13 +6,29 @@ public class Sommatore {
 	}
 	public static int somma10(int x)
 	{
-	return x+10;
+		return x+10;
+	}
+	public static int somma100(int x)
+	{
+		for (int i = 0; i < 100; i++) {
+			x++;
+		}
+		return x;
+	}
+	public static int somma20(int x)
+	{
+		for (int i = 0; i < 20; i++) {
+			x++;
+		}
+		return x;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x=1;
 		System.out.println(somma4(x));
 		System.out.println(somma10(x));
+		System.out.println(somma20(x));
+		System.out.println(somma100(x));
 	}
 
 }
