71aaa5cbea589fe655cef81778db06be31f4a3e0
Tonsi davide <tonsidavide@gmail.com>
2018-02-19 09:11:23 +0100

    ottimizzati somma4 e somma10

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -2,18 +2,17 @@
 public class Sommatore {
 	public static int somma4(int x)
 	{
-		
-		for (int i = 0; i < 4; i++) 
-		{
-			x++;
-		}
-		return x;
+		return x+4;
 	}
-
+	public static int somma10(int x)
+	{
+	return x+10;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x=1;
 		System.out.println(somma4(x));
+		System.out.println(somma10(x));
 	}
 
 }
