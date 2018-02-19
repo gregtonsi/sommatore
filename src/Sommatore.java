089cc3590d9a25f6097d01321d09b20db9225fac
Tonsi davide <tonsidavide@gmail.com>
2018-02-19 09:50:44 +0100

    modificata somma200 in somma1000 e ottimizzata

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -16,12 +16,9 @@ public class Sommatore {
 	{
 		return x+20;
 	}
-	public static int somma200(int x)
+	public static int somma1000(int x)
 	{
-		for (int i = 0; i < 200; i++) {
-			x++;
-		}
-		return x;
+		return x+1000;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -30,7 +27,7 @@ public class Sommatore {
 		System.out.println(somma10(x));
 		System.out.println(somma20(x));
 		System.out.println(somma100(x));
-		System.out.println(somma200(x));
+		System.out.println(somma1000(x));
 	}
 
 }
