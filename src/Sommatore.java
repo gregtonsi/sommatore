0a355c8f3cfa91ad238f2d2382c96619a675f76b
Tonsi davide <tonsidavide@gmail.com>
2018-02-19 09:32:40 +0100

    ottimizzate somma20 e somma 100

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -10,17 +10,11 @@ public class Sommatore {
 	}
 	public static int somma100(int x)
 	{
-		for (int i = 0; i < 100; i++) {
-			x++;
-		}
-		return x;
+		return x+100;
 	}
 	public static int somma20(int x)
 	{
-		for (int i = 0; i < 20; i++) {
-			x++;
-		}
-		return x;
+		return x+20;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
